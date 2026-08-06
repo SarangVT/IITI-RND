d6781001514d1701529ae5c276671cd70ea9f1fe
SarangVT <svdthakare@gmail.com>
2026-08-06 16:28:22 +0530

    Backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,7 +65,7 @@ router.get("/google/callback", async (req, res) => {
   const jwtToken = jwt.sign({ id: user.id, email: user.email, name: user.name }, process.env.JWT_SECRET, { expiresIn: "7d" })
   res.cookie("acKey", jwtToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "PROD",
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
