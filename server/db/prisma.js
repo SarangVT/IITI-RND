56facefc069af8eda571d0b3033690560cbc1683
SarangVT <svdthakare@gmail.com>
2026-08-06 20:30:23 +0530

    Backend

--- server/db/prisma.js
+++ server/db/prisma.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const pool = new Pool({
-  connectionString: (process.env.NODE_ENV="DEV" ? process.env.DEV_DATABASE_URL:process.env.REMOTE_DATABASE_URL),
+  connectionString: (process.env.NODE_ENV=="DEV" ? process.env.DEV_DATABASE_URL:process.env.REMOTE_DATABASE_URL),
 });
 
 const adapter = new PrismaPg(pool);
